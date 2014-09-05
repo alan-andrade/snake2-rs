04e38d03709df0ade545b34fb57d450bfd53ce0b
Alan Andrade <alan.andradec@gmail.com>
2014-09-05 16:13:55 -0700

    Basic Drawable trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+
+static SNAKE: char = '#';
+
+trait Drawable { fn draw(&self); }
+
+struct Snake {
+    skin: char
+}
+
+impl Snake {
+    fn new () -> Snake {
+        Snake { skin: SNAKE }
+    }
+}
+
+impl Drawable for Snake {
+    fn draw (&self) { println!("{}", self.skin); }
+}
+
 fn main() {
-    println!("Hello, world!")
+    let s = Snake::new();
+    s.draw();
 }
