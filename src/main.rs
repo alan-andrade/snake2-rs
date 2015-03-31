e8ba68ba85957e8911911e1a6eeb1f3c53e15b2b
Alan Andrade <alan.andradec@gmail.com>
2015-03-30 18:43:28 -0700

    Dust out this snake game

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-
-static SNAKE: char = '#';
-
-trait Drawable { fn draw(&self); }
-
-struct Snake {
-    skin: char
-}
-
-impl Snake {
-    fn new () -> Snake {
-        Snake { skin: SNAKE }
-    }
-}
-
-impl Drawable for Snake {
-    fn draw (&self) { println!("{}", self.skin); }
-}
-
-fn main() {
-    let s = Snake::new();
-    s.draw();
+fn main () {
+    println!("Implement a snake game here")
 }
