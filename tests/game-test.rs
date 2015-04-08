454d3ed539884d2b94e2cd807aa24f1110f5443f
Alan Andrade <alan.andradec@gmail.com>
2015-04-08 09:34:16 -0700

    refactors Game allocate_object_at -> add_object_at

--- tests/game-test.rs
+++ tests/game-test.rs
@@ -13,35 +13,19 @@ struct Game<'a, T: 'a> {
     grid: &'a mut Grid<T>
 }
 
-impl<'a, T> Game<'a, T> where T: Collidable, T: Copy {
-    fn allocate_at(&mut self, position: Position, object: T) -> AllocationEvent<T> {
+#[derive(Debug, PartialEq, Eq)]
+enum GameEvent {
+    Crash,
+    ObjectAdded
+}
+
+impl<'a, T> Game<'a, T> {
+    fn add_object_at(&mut self, object: T, position: Position) -> GameEvent {
         match self.grid.allocate_object_at(object, position) {
-            AllocationEvent::Collition(obstacle) => {
-                object.handle_collition(obstacle)
-            },
-
-            AllocationEvent::Allocated => {
-                AllocationEvent::Allocated
-            },
-
-            _ => { AllocationEvent::Collition(object) }
+            AllocationEvent::Collition(obstacle) => { GameEvent::Crash },
+            AllocationEvent::Allocated => { GameEvent::ObjectAdded },
+            _ => { GameEvent::Crash }
         }
-    }
-}
-
-trait Collidable {
-    fn handle_collition(&self, Self) -> AllocationEvent<Self>;
-}
-
-impl Collidable for Object {
-    fn handle_collition(&self, obstacle: Object) -> AllocationEvent<Object> {
-        //match (self, obstacle) {
-            //(&Object::Snake, Object::Snake) |
-            //(&Object::Snake, Object::Wall) => {return AllocationEvent::Crash; }
-            //(&Object::Snake, Object::Apple) => { return AllocationEvent::Yum; }
-            //(_, _) => { return AllocationEvent::CollitionRuleMissing }
-        //}
-        AllocationEvent::Collition(obstacle)
     }
 }
 
@@ -51,11 +35,11 @@ fn game_has_a_grid() {
     let mut game = Game { grid: &mut grid };
     let (wall, apple, snake) = (Object::Wall, Object::Apple, Object::Snake);
 
-    assert_eq!(game.allocate_at(Position(1, 1), wall), AllocationEvent::Allocated);
-    assert_eq!(game.allocate_at(Position(1, 2), apple), AllocationEvent::Allocated);
-    //assert_eq!(game.allocate_at(Position(1, 2), snake), AllocationEvent::Yum);
-    //assert_eq!(game.allocate_at(Position(1, 1), snake), AllocationEvent::Crash);
-    assert_eq!(game.allocate_at(Position(1, 3), snake), AllocationEvent::Allocated);
-    //assert_eq!(game.allocate_at(Position(1, 3), snake), AllocationEvent::Crash);
-    //assert_eq!(game.allocate_at(Position(1, 1), apple), AllocationEvent::CollitionRuleMissing);
+    assert_eq!(game.add_object_at(wall, Position(1, 1)), GameEvent::ObjectAdded);
+    assert_eq!(game.add_object_at(apple, Position(1, 2)), GameEvent::ObjectAdded);
+    //assert_eq!(game.add_object_at(Position(1, 2), snake), AllocationEvent::Yum);
+    //assert_eq!(game.add_object_at(Position(1, 1), snake), AllocationEvent::Crash);
+    assert_eq!(game.add_object_at(snake, Position(1, 3)), GameEvent::ObjectAdded);
+    //assert_eq!(game.add_object_att(Position(1, 3), snake), AllocationEvent::Crash);
+    //assert_eq!(game.add_object_att(Position(1, 1), apple), AllocationEvent::CollitionRuleMissing);
 }
