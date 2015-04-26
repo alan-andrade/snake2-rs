7b4e163fe450761da034028515c5f515062e1b84
Alan Andrade <alan.andradec@gmail.com>
2015-04-26 13:04:39 -0700

    setup objects

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,19 @@ pub enum Object {
     Wall
 }
 
+impl Object {
+    fn foo() {}
+}
+
 enum Direction {
     Up,
     Right,
     Down,
     Left
 }
+
+#[derive(Debug, PartialEq, Eq)]
+struct Player(Position, Object);
 
 fn navigate(position: &Position, direction: Direction) -> Position {
     let Position(x, y) = *position;
@@ -27,60 +34,79 @@ fn navigate(position: &Position, direction: Direction) -> Position {
     }
 }
 
-struct Stage;
+fn setup_walls(grid: &mut Grid<Object>) {
 
-impl Stage {
-    fn setup(grid: &mut Grid<Object>) {
-        // setup walls
-        let mut collector = vec!();
+    let mut to_be_occupied = vec!();
 
-        {
-            let mut top_wall = Position(1, 1);
-            let mut bottom_wall = Position(1, grid.height);
+    // XXX: Lazy generate positions
+    {
+        let mut top_wall = Position(1, 1);
+        let mut bottom_wall = Position(1, grid.height);
 
-            for _ in 1..grid.width {
-                collector.push(top_wall);
-                top_wall = navigate(&top_wall, Direction::Right);
+        for _ in (1..grid.width + 1) {
+            to_be_occupied.push(top_wall);
+            top_wall = navigate(&top_wall, Direction::Right);
 
-                collector.push(bottom_wall);
-                bottom_wall = navigate(&bottom_wall, Direction::Right);
-            }
+            to_be_occupied.push(bottom_wall);
+            bottom_wall = navigate(&bottom_wall, Direction::Right);
         }
+    }
 
-        {
-            let mut left_wall = Position(1, 1);
-            let mut right_wall= Position(grid.width, 1);
+    {
+        let mut left_wall = Position(1, 1);
+        let mut right_wall= Position(grid.width, 1);
 
-            for _ in 1..grid.height {
-                collector.push(left_wall);
-                left_wall = navigate(&left_wall, Direction::Down);
+        for _ in (1 .. grid.height + 1) {
+            to_be_occupied.push(left_wall);
+            left_wall = navigate(&left_wall, Direction::Down);
 
-                collector.push(right_wall);
-                right_wall = navigate(&right_wall, Direction::Down);
-            }
+            to_be_occupied.push(right_wall);
+            right_wall = navigate(&right_wall, Direction::Down);
         }
+    }
 
-        for pos in collector {
-            grid.allocate_object_at(Object::Wall, pos);
-        }
-
-        //let center = grid.center();
-        //for _ in (1..4) {
-            //position = match grid.allocate_object_at(Snake, position) {
-                //Allocated => navigate(&position, Direction::Up),
-                //_ => panic!("Position {} is not available, Couldn't allocate snake", position)
-            //}
-        //}
+    for position in to_be_occupied {
+        grid.allocate_object_at(Object::Wall, position);
     }
 }
 
+fn setup_snake(grid: &mut Grid<Object>) {
+    use grid::AllocationEvent::Allocated;
+
+    let mut position = grid.center();
+    for _ in (1..4) {
+        position = match grid.allocate_object_at(Object::Snake, position) {
+            Allocated => navigate(&position, Direction::Up),
+            _ => panic!("Position {} is not available, Couldn't allocate snake", position)
+        }
+    }
+}
+
+fn setup_grid(grid: &mut Grid<Object>) {
+    setup_walls(grid);
+    setup_snake(grid);
+}
 
 #[test]
-fn test_stage_setup() {
-    let mut grid = Grid::new(4, 4);
-    Stage::setup(&mut grid);
+fn test_setup_walls() {
+    let mut grid = Grid::new(5, 5);
+    setup_walls(&mut grid);
+    assert_eq!(grid.object_at(Position(1, 1)), Some(&Object::Wall));
+    assert_eq!(grid.object_at(Position(5, 5)), Some(&Object::Wall));
+    assert_eq!(grid.object_at(Position(1, 5)), Some(&Object::Wall));
+    assert_eq!(grid.object_at(Position(5, 1)), Some(&Object::Wall));
+    assert_eq!(grid.object_at(Position(3, 3)), None);
+}
+
+#[test]
+fn test_setup_grid() {
+    let mut grid = Grid::new(10, 10);
+
+    setup_grid(&mut grid);
 
     assert_eq!(grid.object_at(Position(1, 1)), Some(&Object::Wall));
+    let center = grid.center();
+    assert_eq!(grid.object_at(center), Some(&Object::Snake));
 }
 
 fn main () { }
